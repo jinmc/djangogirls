7a4cf1b8781d777df9d44524efdef9af57d18ad4
Jim Chong <jinmo55555@gmail.com>
2020-05-20 03:18:20 -0400

    add detail page href and pages

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.utils import timezone
 
@@ -9,4 +9,10 @@ def post_list(request):
     qs = qs.order_by('-published_date')
     return render(request, 'blog/post_list.html', {
         'post_list' : qs
+    })
+
+def post_detail(request, pk):
+    qs = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_Detail.html', {
+        'post': qs
     })

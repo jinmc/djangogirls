a718b1b246c53a9a89e3a586e390060feb5600a9
Jim Chong <jinmo55555@gmail.com>
2020-05-20 01:19:17 -0400

    make post_list function from views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('-published_date')
+    return render(request, 'blog/post_list.html', {
+        'post_list' : qs
+    })

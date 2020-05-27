c2392be6adda64ba3a7efd592c98240410ab483e
Jim Chong <jinmo55555@gmail.com>
2020-05-27 18:29:03 -0400

    make jobkorea posting

--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,21 @@ class Job_Heykorean(models.Model):
 
     def str(self):
         return " ".join([company_name, job_description, str(published_date)])
+
+class Job_JobKorea(models.Model):
+    company_name = models.CharField(max_length=200)
+    company_etc = models.CharField(max_lenght=200, null=True)
+    job_title = models.CharField(max_length=200)
+    job_description = models.TextField()
+    post_link = models.CharField(max_length=200)
+    published_date = models.DateTimeField(blank=True, null=True)
+    area = models.CharField(max_length=200, null=True)
+    jobstyle = models.CharField(max_length=200, null=True)
+    deadline = models.DateField(blank=True, null=True)
+    salary = models.CharField(max_length=200, null=True)
+    eduction = models.CharField(max_length=200, null=True)
+    experience = models.CharField(max_length=200, null=True)
+    etc = models.CharField(max_length=200, null=True)
+
+    def str(self):
+        return " ".join([company_name, job_description, str(published_date)])
---
+++ utils/scrape_jobkorea.py
@@ -0,0 +1,141 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+import os, sys
+from datetime import date
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+url = 'http://www.jobkorea.co.kr/recruit/joblist?menucode=duty&dutyCtgr=10016'
+page_count = 1
+
+count = 0
+for i in range(10):
+    if i == 1:
+        url_target = url
+    else:
+        url_target = url + 'anchorGICnt_' + str(i)
+
+    source = requests.get(url_target).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    general = soup.find('div', id='dev-gi-list')
+
+    for job in general.find_all('tr', class_='devloopArea'):
+    # print(job)
+        tplco = job.find('td', class_='tplCo') # company name
+        company_name = tplco.find('a').text
+        count += 1
+        titBx = job.find('div', class_='titBx') # job title, desc
+        job_title = titBx.find('a').text
+        job_description = titBx.find('p', class_='dsc').text
+        post_link = 'http://www.jobkorea.co.kr/' + titBx.find('a').get('href')
+        etcs = titBx.find('p', class_='etc').find_all('span')
+        # print(etcs)
+        experience =  etcs.pop(0).text.replace('외', '').strip()
+        education = etcs.pop(0).text.replace('외', '').strip()
+        area = etcs.pop(0).text.replace('외', '').strip()
+        jobstyle = etcs.pop(0).text.replace('외', '').strip()
+        if etcs:
+            salary = etcs.pop(0).text.replace('외', '').strip()
+        odd = job.find('td', class_='odd')
+        # print(odd)
+        deadline = odd.find('span', class_='date').text.replace('~','')
+        deadline = deadline.split('(')[0]
+        # print(deadline)
+
+        published_date = odd.find('span', class_='time').text.replace('등록', '').strip()
+        # print(published_date)
+
+        # print(titBx)
+        # print(tplco)
+        print()
+        print(company_name, count)
+        print(job_title)
+        print(job_description)
+        print(post_link)
+        print(published_date)
+        print(area)
+        print(jobstyle)
+        print(deadline)
+        print(salary)
+        print(experience)
+        print(education)
+        # print(etc)
+        # break
+    print(f'count : {count}')
+
+
+print("dying now...")
+sys.exit()
+print("this message will not print")
+
+
+# scrape jobkorea's it jobs
+source = requests.get('http://www.jobkorea.co.kr/recruit/joblist?menucode=duty&dutyCtgr=10016').text
+soup = BeautifulSoup(source, 'lxml')
+
+dbfile = os.path.join(BASE_DIR, 'db.sqlite3')
+print(dbfile)
+conn = sqlite3.connect(dbfile)
+c = conn.cursor()
+# c.execute('DELETE FROM blog_job_jobkorea;')
+
+# print(soup.prettify())
+# all jobs
+general = soup.find('div', id='dev-gi-list')
+# print(general)
+
+count = 0
+for job in general.find_all('tr', class_='devloopArea'):
+    # print(job)
+    tplco = job.find('td', class_='tplCo') # company name
+    company_name = tplco.find('a').text
+    count += 1
+    titBx = job.find('div', class_='titBx') # job title, desc
+    job_title = titBx.find('a').text
+    job_description = titBx.find('p', class_='dsc').text
+    post_link = 'http://www.jobkorea.co.kr/' + titBx.find('a').get('href')
+    etcs = titBx.find('p', class_='etc').find_all('span')
+    # print(etcs)
+    experience =  etcs.pop(0).text.replace('외', '').strip()
+    education = etcs.pop(0).text.replace('외', '').strip()
+    area = etcs.pop(0).text.replace('외', '').strip()
+    jobstyle = etcs.pop(0).text.replace('외', '').strip()
+    if etcs:
+        salary = etcs.pop(0).text.replace('외', '').strip()
+    odd = job.find('td', class_='odd')
+    # print(odd)
+    deadline = odd.find('span', class_='date').text.replace('~','')
+    deadline = deadline.split('(')[0]
+    # print(deadline)
+
+    published_date = odd.find('span', class_='time').text.replace('등록', '').strip()
+    # print(published_date)
+
+    # print(titBx)
+    # print(tplco)
+    print()
+    # print(deadline)
+    # print(published_date)
+    # print(experience)
+    # print(education)
+    # print(area)
+    # print(jobstyle)
+    # print(salary)
+    # print(post_link)
+    # print(job_description)
+    # print(company_name, count)
+    # print(job_title)
+    # break
+print(f'count : {count}')
+'''
+for article in soup.find('div', id='dev-gi-list'):
+    # print(article.find('div', class_='company-title').text.strip()) # company title
+    # print(article.find('span', class_='date-created').text.strip()) # date created
+    # print(article.find('a', class_='job-title').text.strip()) # job title
+    # print(''.join(['job.heykorean.com', article.find('a').get('href')])) # make link 
+    print(article.prettify()) # to see everything
+    print()
+'''
